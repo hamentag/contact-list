7665f0257e62ec75b4b9a5a611dbf06708cfd621
Hamza Amentag <hamentag1@gmail.com>
2024-02-16 12:42:15 -0600

    updated: add ContactList and ContactRow components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import ContactList from './components/ContactList'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <ContactList />
     </>
   )
 }
---
+++ src/components/ContactRow.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function ContactRow({contact}){
+    return(
+       <tr>
+        <td>{contact.name}</td>
+        <td>{contact.phone}</td>
+        <td>{contact.email}</td>
+       </tr>
+    );    
+}
